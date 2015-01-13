8d9575c84f347c73aa33add7d2ef492ee449b862
Old Gobbo <ramnikarora@gmail.com>
2015-01-13 00:27:59 -0500

    Added additional functionality to the Guessing Game, such as lower and upper bounds, reset button and so on

--- js/gg.js
+++ js/gg.js
@@ -1,70 +1,126 @@
 // Library functions
 String.prototype.toNum = function() {
 	return parseInt(this, 10);
-}
+};
 
-// User-defined variable initialization
-var $tryCount = 0;
+// Constants
 var $maximumNumberOfTries = 3;
-var $minValue = 0;
-var $maxValue = 100;
-var $midValue = 0;
-var $inputValue = 0;
-var $randomNumberGenerated = Math.floor(Math.random() * 100);
-
-var $numTriesLeft = $maximumNumberOfTries - $tryCount;
-var $canSubmit = $numTriesLeft > 0;
 
 var $resetButton = $("#resetButton");
 var $submitButton = $("#submitButton");
 var $inputField = $("#inputNumber");
 
-// Updaters
-var updateMidValue = function () {
-	midValue = Math.floor(($minValue + $maxValue) / 2);
-}
+// User-defined variable initialization
+var $tryCount;
+var $minValue;
+var $maxValue;
+var $midValue;
+var $inputValue;
+var $randomNumberGenerated;
+var $numTriesLeft;
+var $stringVal;
+var $canSubmit;
 
+// Updaters
 var updateNumTriesLeft = function() {
 	$numTriesLeft = $maximumNumberOfTries - $tryCount;
-}
-
-$resetButton.hide();
-
-var $toggleResetButton = function() {
-	if ($tryCount > 0) $resetButton.show()
-	else $resetButton.hide();
 };
 
-var $resetInputValue = function() {
-	$inputValue = $inputField.val().toNum();
-}
+var generateNewRandomNumber = function() {
+	$randomNumberGenerated = Math.floor(Math.random() * 100);
+};
 
-var $flushButtonValue = function() {
+var updateMinValue = function() {
+	$minValue = ($inputValue > $randomNumberGenerated) ? $minValue: $inputValue;
+	updateMidValue();
+};
+
+var updateMaxValue = function() {
+	$maxValue = ($inputValue < $randomNumberGenerated) ? $maxValue : $inputValue;
+	updateMidValue();
+};
+
+var updateMidValue = function () {
+	$midValue = Math.floor(($minValue + $maxValue) / 2);
+};
+
+var updateCanSubmit = function() {
+	$canSubmit = $numTriesLeft > 0 && !($inputValue === $randomNumberGenerated);
+	if (!$canSubmit)
+		$submitButton.attr("disabled", true);
+	else
+		$submitButton.attr("disabled", false);
+};
+
+// Reset button toggle
+var toggleResetButton = function() {
+	if ($tryCount > 0) 
+		$resetButton.show();
+	else 
+		$resetButton.hide();
+};
+
+var getInputValue = function() {
+	$inputValue = $inputField.val().toNum();
+};
+
+var flushButtonValue = function() {
 	console.log($inputValue);
-	$inputField.attr("min", (Math.min($inputValue, $minValue)).toString());
-	$inputField.attr("max", (Math.min($inputValue, $maxValue)).toString());
+	updateMinValue();
+	updateMaxValue();
+	updateMidValue();
+	$inputField.attr("min", $minValue.toString());
+	$inputField.attr("max", $maxValue.toString());
 	$inputField.attr("value", $midValue.toString());
 };
 
-var $incrementTryCount = function() {
+var incrementTryCount = function() {
 	$tryCount++;
+	updateNumTriesLeft();
 };
 
-var $outputSignal = function() {
+var updateOutputString = function() {
+	if ($tryCount > 0) {
+		if ($inputValue > $randomNumberGenerated)
+			$stringVal = "Hot - Lower your guess";
+		else if ($inputValue < $randomNumberGenerated)
+			$stringVal = "Cold - Increase your guess";
+		else if ($inputValue === $randomNumberGenerated)
+			$stringVal = "Success";
+		else if ($numTriesLeft <= 0)
+			$stringVal = "Sorry";
+	} else {
+		$stringVal = "";
+	}
 };
 
 $submitButton.click(function(event) {
 	event.preventDefault();
-	$resetInputValue();
-	$incrementTryCount();
-	$toggleResetButton();
-	$flushButtonValue();
+	getInputValue();
+	incrementTryCount();
+	toggleResetButton();
+	flushButtonValue();
+	updateOutputString();
+	updateCanSubmit();
 });
 
-var $flushVariables = function() {
+$resetButton.click(function(event) {
+	event.preventDefault();
+	flushVariables();	
+	flushButtonValue();
+});
+
+var flushVariables = function() {
 	$tryCount = 0;
 	$minValue = 0;
 	$maxValue = 99;
 	$inputValue = 0;
-	$randomNumberGenerated = Math.floor(Math.random() * 100);
+	$stringVal = "";
+	generateNewRandomNumber();	
+	updateNumTriesLeft();
+	toggleResetButton();
+	updateOutputString();
+	updateCanSubmit();
 }
+
+flushVariables();
